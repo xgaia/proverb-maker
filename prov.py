604b038250182c0bb6a8959d5a8cbcc2285ad37e
root <root@proverbme.novalocal>
2017-11-10 14:38:37 +0000

    Parse wiki

--- prov.py
+++ prov.py
@@ -2,19 +2,50 @@
 import random
 import locale
 import os
+import re, json, sys
+from urllib2 import Request, urlopen, URLError
+from bs4 import BeautifulSoup
 
 import polyglot
 from polyglot.text import Text, Word
 
+adress_dict={"fr":"https://fr.wiktionary.org/w/api.php?titles=Annexe:Liste_de_proverbes_fran%C3%A7ais"
+,"en":"https://en.wiktionary.org/w/api.php?titles=Appendix:English_proverbs"}
+
+def get_list(lang="fr"):
+    if not lang == "fr" and not lang == "en":
+        print "Only support fr & en languages"
+        sys.exit(1)
+    request = Request(adress_dict[lang]+"&action=query&prop=extracts&format=json")
+    response = urlopen(request)
+    body = json.loads(response.read())["query"]["pages"]
+    for key in body.keys():
+    	return body[key]["extract"]
+
+def parse_list(lang="fr"):
+    if not lang == "fr" and not lang == "en":
+        print "Only support fr & en languages"
+        sys.exit(1)
+    html_string = get_list(lang)
+    parsed_html = BeautifulSoup(html_string,"lxml")
+    proverb_list = []
+    if lang == "fr":
+        references = parsed_html.find("h3")
+        for elm in references.find_next_siblings():
+            elm.extract()
+        references.extract()
+        for pro in parsed_html.find_all('li'):
+            proverb_list.append(pro.get_text().rstrip("."))
+    elif lang == "en":
+        for pro in parsed_html.find_all('dd'):
+            proverb_list.append(pro.get_text().rstrip("."))
+    return proverb_list
 
 def build_proverbs_file(lang):
     """Build a file with proverbs and cut them with a @"""
 
     # Get uncut proverbs file
-    #TODO: parse wikipedia page to get uncut proverbs
-    file_path = 'prov_%s_uncut.txt' % lang
-    file = open(file_path)
-    uncut_provs = file.readlines()
+    uncut_provs = parse_list(lang)
 
     # Open results file (delete if exist)
     resfile_path = 'prov_%s.txt' % lang
@@ -91,9 +122,9 @@ def main():
     file_path = 'prov_%s.txt' % lang
 
     # Make the prov file if not exist
+    
     if not os.path.isfile(file_path):
-        build_proverbs_file(lang) 
-
+        build_proverbs_file(lang)
     # Get proverbs
     file = open(file_path)
     prov = file.readlines()
